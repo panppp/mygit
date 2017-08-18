1dd2cbb9ec922ad747981df6ae6b30b4deed3c3a
panppp <763194840@qq.com>
2017-08-18 21:38:38 +0800

    canvas

---
+++ canvas_text/DOM存储/js/2.js
@@ -0,0 +1,40 @@
+/**
+一个很耗时的JS：
+复杂的算法：天量的枚举和遍历、嵌套、递归
+例如：大数的质数判定
+**/
+
+var num = 92929138418911121;
+console.time('质数判定计时');
+var result = isPrime(num);
+console.timeEnd('质数判定计时');
+//console.log(num+'是质数吗：'+result);
+
+//window.alert(num+'是质数吗：'+result);
+//document.getElementById('p1').innerHTML = num+'是质数吗：'+result;
+//p1.innerHTML = num+'是质数吗：'+result;
+
+function isPrime(num){
+  console.log('开始进行质数判定...');
+  var result = false;
+
+  //让CPU疯狂的执行5s的死循环
+  var start = new Date().getTime();
+  do{
+    var now = new Date().getTime();
+  }while( now-start<5000 );
+
+  for(var i=2; i<num; i++){
+    if(num%i===0){
+      break;
+    } 
+  }
+  if(i===num){
+    result = true;
+  }else {  
+    result = false;
+  }
+
+  console.log('质数判定执行结束...');
+  return result;
+}
